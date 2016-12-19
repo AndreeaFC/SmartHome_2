238c0a90f68cc538bafc1a51f50b0ffc3d1fe0e8
AndreeaFC <andreea_vhr@yahoo.com>
2016-12-19 23:27:02 +0100

    minor last changes

--- SmartHomeProject/Scripts/HouseArea/HouseArea.js
+++ SmartHomeProject/Scripts/HouseArea/HouseArea.js
@@ -10,7 +10,6 @@ var rooms = [
 ]
 
 $(document).ready(function () {
-   
     GetHouseArea();
 
     //$('#light-switch-Livingroom').change(function () {
@@ -22,6 +21,7 @@ $(document).ready(function () {
     //        updateHouseArea(room, false)
     //    }
     //});
+
     console.log("ready!");
     var hubbie = $.connection.myHub;
     $.connection.hub.logging = true;
@@ -35,23 +35,16 @@ $(document).ready(function () {
         if (status == "on") {
             $("#text-" + room).text(room);
             $("#text-" + room).append(on);
-            //$('#light-switch-' + room).val(true);
-            //var id = "#light-switch-" + room;
-            //document.getElementById("light-switch-" + room).checked = true;
             $('#light-switch-' + room).prop('checked', true);
-            
+            //$("#light-switch-" + room).click();
         }
         else {
             $("#text-" + room).text(room);
             $("#text-" + room).append(off);
-            //$('#light-switch-' + room).val(false);
-            //document.getElementById("light-switch-" + room).checked = false;
             $('#light-switch-' + room).prop('checked', false);
-        }
-        
+        }  
     };
     $.connection.hub.start();
-    
 });
 
 function GetHouseArea() {
@@ -67,7 +60,6 @@ function GetHouseArea() {
         error: function (x, y, z) {
             alert(x + '\n' + y + '\n' + z);
             console.log(houseArea);
-            //houseAreas = {};
         }
     });
 }
@@ -80,7 +72,7 @@ function findHouseArea(inputName) {
 
 function updateHouseArea(areaName, lightsOn, floorHeating, alarm) {
     jQuery.support.cors = true;
-    findHouseArea(areaName); //gives us house area from db that we stored in array up there
+    findHouseArea(areaName);
     var thisHouseArea = {
         Id: houseArea.Id,
         AreaName: areaName,
@@ -102,23 +94,19 @@ function updateHouseArea(areaName, lightsOn, floorHeating, alarm) {
     });
 }
 
-function AddHouseArea() {
+function DeleteHouseArea() {
     jQuery.support.cors = true;
-    var houseArea = {
-        ID: $('#txtaddHouseAreaId').val(),
-        AreaName: $('#txtaddName').val(),
-        LightsOn: $('#txtaddLightsOn').val(),
-        FloorHeating: $('#txtaddFloorHeating').val(),
-        Alarm: $('#txtaddAlarm').val()
-    };
+    var id = $('#deleteId').val();
 
     $.ajax({
-        url: 'http://localhost:53046/api/HouseAreaAPI/',
-        type: 'POST',
-        data: JSON.stringify(houseArea),
+        url: 'http://localhost:53046/api/HouseAreaAPI/' + id,
+        type: 'DELETE',
         contentType: "application/json;charset=utf-8",
         success: function (data) {
-            WriteResponse(data);
+            GetHouseArea();
+            console.log(houseAreas);
+            alert("Area deleted!");
+            //WriteResponse(data);
         },
         error: function (x, y, z) {
             alert(x + '\n' + y + '\n' + z);
@@ -126,18 +114,19 @@ function AddHouseArea() {
     });
 }
 
-//function UppdateHouseArea() {
+//function AddHouseArea() {
 //    jQuery.support.cors = true;
 //    var houseArea = {
-//        ID: $('#txtUpdateHouseAreaId').val(),
-//        Name: $('#txtUpdateName').val(),
-//        LightsOn: $('#txtUpdateLightsOn').val(),
-//        FloorHeating: $('#txtUpdateFloorHeating').val()
+//        ID: $('#txtaddHouseAreaId').val(),
+//        AreaName: $('#txtaddName').val(),
+//        LightsOn: $('#txtaddLightsOn').val(),
+//        FloorHeating: $('#txtaddFloorHeating').val(),
+//        Alarm: $('#txtaddAlarm').val()
 //    };
 
 //    $.ajax({
-//        url: 'http://localhost:53046/api/HouseAreaAPI/' + houseArea.ID,
-//        type: 'PUT',
+//        url: 'http://localhost:53046/api/HouseAreaAPI/',
+//        type: 'POST',
 //        data: JSON.stringify(houseArea),
 //        contentType: "application/json;charset=utf-8",
 //        success: function (data) {
@@ -149,22 +138,5 @@ function AddHouseArea() {
 //    });
 //}
 
-//function DeleteHouseArea() {
-//    jQuery.support.cors = true;
-//    var id = $('#deleteId').val();
 
-//    $.ajax({
-//        url: 'http://localhost:53046/api/HouseAreaAPI/' + id,
-//        type: 'DELETE',
-//        contentType: "application/json;charset=utf-8",
-//        success: function (data) {
-//            GetHouseArea();
-//            console.log(houseAreas);
-//            //WriteResponse(data);
-//        },
-//        error: function (x, y, z) {
-//            alert(x + '\n' + y + '\n' + z);
-//        }
-//    });
-//}
 
